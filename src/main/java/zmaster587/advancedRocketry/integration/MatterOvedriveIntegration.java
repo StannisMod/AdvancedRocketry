459a8d73745e77be9a294fb972fb9b7d1e1ccf8d
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2025-04-13 16:45:25 +0200

    https://discord.com/channels/1251616570568867932/1251623407624261702/1360982573756584006

--- src/main/java/zmaster587/advancedRocketry/integration/MatterOvedriveIntegration.java
+++ src/main/java/zmaster587/advancedRocketry/integration/MatterOvedriveIntegration.java
@@ -2,7 +2,7 @@ package zmaster587.advancedRocketry.integration;
 
 import matteroverdrive.entity.*;
 import matteroverdrive.entity.monster.EntityMeleeRougeAndroidMob;
-import matteroverdrive.entity.monster.EntityRougeAndroidMob;
+import matteroverdrive.entity.monster.EntityRangedRogueAndroidMob;
 import matteroverdrive.entity.player.MOPlayerCapabilityProvider;
 import matteroverdrive.init.OverdriveBioticStats;
 import net.minecraft.entity.EntityLivingBase;
@@ -19,7 +19,7 @@ public class MatterOvedriveIntegration {
         // for some reason AR cant register MO entities to bypass, so we just make this a dummy method
 
         arConfig.bypassEntity.add(EntityDrone.class);
-        arConfig.bypassEntity.add(EntityRougeAndroidMob.class);
+        arConfig.bypassEntity.add(EntityRangedRogueAndroidMob.class);
         arConfig.bypassEntity.add(EntityMeleeRougeAndroidMob.class);
     }
 }
