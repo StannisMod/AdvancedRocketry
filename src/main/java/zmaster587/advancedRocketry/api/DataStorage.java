f593ad400d2e85e00f1330869f6ebe79f14e5845
kaduvill <johansj@uia.no>
2025-10-30 11:59:53 +0100

    Enhance data type handling in DataStorage
    Refactor data handling in DataStorage class to improve type management and data addition logic.

--- src/main/java/zmaster587/advancedRocketry/api/DataStorage.java
+++ src/main/java/zmaster587/advancedRocketry/api/DataStorage.java
@@ -20,15 +20,24 @@ public class DataStorage {
     }
 
     public boolean setData(int data, DataType dataType) {
-        if (this.dataType == DataStorage.DataType.UNDEFINED)
+        // If empty/typeless, allow adopting the provided type (unless locked)
+        if (this.dataType == DataStorage.DataType.UNDEFINED && !this.locked) {
             this.dataType = dataType;
+        }
 
         if (dataType == DataStorage.DataType.UNDEFINED || dataType == this.dataType) {
             this.data = Math.min(data, maxData);
+
+            // If we just became empty and are not locked, clear type
+            if (!this.locked && this.data == 0) {
+                this.dataType = DataType.UNDEFINED;
+            }
             return true;
         }
         return false;
     }
+
+
 
     public int getData() {
         return data;
@@ -78,19 +87,45 @@ public class DataStorage {
      * @param dataType type to add
      * @return data amount added
      */
-    public int addData(int data, DataType dataType, boolean commit) {
-        if ((!this.locked && (dataType == DataStorage.DataType.UNDEFINED)) || dataType == this.dataType || this.dataType == DataStorage.DataType.UNDEFINED) {
+    public int addData(int data, DataType incomingType, boolean commit) {
+        // Snapshot
+        final boolean empty = (this.data == 0);
+        DataType current = this.dataType;
 
-            if (this.dataType == DataStorage.DataType.UNDEFINED)
-                this.dataType = dataType;
-
-            int amountToAdd = Math.min(data, this.maxData - this.data);
-            if (commit)
-                this.data += amountToAdd;
-            return amountToAdd;
+        // Compute effective type without mutating state on simulation
+        DataType effective = current;
+        if (!this.locked && empty) {
+            effective = (incomingType != DataType.UNDEFINED) ? incomingType : DataType.UNDEFINED;
         }
-        return 0;
+
+        // Accept if: unlocked+UNDEFINED (wildcard), same type, or typeless
+        boolean accepts =
+            (!this.locked && incomingType == DataType.UNDEFINED) ||
+            (incomingType == effective) ||
+            (!this.locked && effective == DataType.UNDEFINED);
+
+
+        if (!accepts) return 0;
+
+        int amountToAdd = Math.min(data, this.maxData - this.data);
+        if (amountToAdd <= 0) return 0;
+
+        if (commit) {
+            // Finalize adoption only on real add
+            if (!this.locked && this.data == 0) {
+                this.dataType = (incomingType != DataType.UNDEFINED) ? incomingType : DataType.UNDEFINED;
+            }
+            this.data += amountToAdd;
+
+            // If still UNDEFINED but we added concrete data (edge case), solidify
+            if (this.dataType == DataType.UNDEFINED && incomingType != DataType.UNDEFINED) {
+                this.dataType = incomingType;
+            }
+        }
+        return amountToAdd;
     }
+
+
 
     /**
      * @param data max amount of data to remove
@@ -122,13 +157,17 @@ public class DataStorage {
             dataType = DataType.UNDEFINED;
         }
 
-
-        ///TODO: dev compat
         if (nbt.hasKey("locked"))
             locked = nbt.getBoolean("locked");
         else
             locked = false;
+
+        // >>> ADD: heal stale type on empty <<<
+        if (!locked && data == 0) {
+            dataType = DataType.UNDEFINED;
+        }
     }
+
 
     public enum DataType {
         UNDEFINED,
