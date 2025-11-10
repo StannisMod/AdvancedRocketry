6802d853950fc3cb5d38aec5bf1dcde97c788747
kaduvill <johansj@uia.no>
2025-11-10 12:52:32 +0100

    Refactor tooltip information handling in BlockLandingPad

--- src/main/java/zmaster587/advancedRocketry/block/BlockLandingPad.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockLandingPad.java
@@ -20,6 +20,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import zmaster587.advancedRocketry.client.TooltipInjector;
 import zmaster587.advancedRocketry.tile.station.TileLandingPad;
 import zmaster587.libVulpes.LibVulpes;
 import zmaster587.libVulpes.inventory.GuiHandler;
@@ -62,41 +63,8 @@ public class BlockLandingPad extends Block {
     @SideOnly(Side.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag) {
-        tooltip.add(TextFormatting.GRAY + I18n.format("tooltip.advancedrocketry.landingpad"));
-
-        final boolean shift = GuiScreen.isShiftKeyDown();
-        final boolean alt   = isAltDown();
-
-        if (alt) {
-            // Advanced details
-            tooltip.add(TextFormatting.DARK_GRAY + I18n.format("tooltip.advancedrocketry.landingpad.alt.1"));
-            tooltip.add(TextFormatting.DARK_GRAY + I18n.format("tooltip.advancedrocketry.landingpad.alt.2"));
-            tooltip.add(TextFormatting.DARK_GRAY + I18n.format("tooltip.advancedrocketry.landingpad.alt.3"));
-        } else if (shift) {
-            // More info
-            tooltip.add(TextFormatting.GRAY + I18n.format("tooltip.advancedrocketry.landingpad.shift.1"));
-            tooltip.add(TextFormatting.GRAY + I18n.format("tooltip.advancedrocketry.landingpad.shift.2"));
-            if (I18n.hasKey("tooltip.advancedrocketry.hold_alt"))
-                tooltip.add(TextFormatting.DARK_GRAY.toString() + TextFormatting.ITALIC + I18n.format("tooltip.advancedrocketry.hold_alt"));
-        } else {
-            // Hints
-            if (I18n.hasKey("tooltip.advancedrocketry.hold_shift"))
-                tooltip.add(TextFormatting.DARK_GRAY.toString() + TextFormatting.ITALIC + I18n.format("tooltip.advancedrocketry.hold_shift"));
-            if (I18n.hasKey("tooltip.advancedrocketry.hold_alt"))
-                tooltip.add(TextFormatting.DARK_GRAY.toString() + TextFormatting.ITALIC + I18n.format("tooltip.advancedrocketry.hold_alt"));
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    private static boolean isAltDown() {
-        try {
-            // Works on Forge 1.12.x; LWJGL fallback for safety
-            return GuiScreen.isAltKeyDown()
-                || org.lwjgl.input.Keyboard.isKeyDown(org.lwjgl.input.Keyboard.KEY_LMENU)
-                || org.lwjgl.input.Keyboard.isKeyDown(org.lwjgl.input.Keyboard.KEY_RMENU);
-        } catch (Throwable t) {
-            return false;
-        }
+        int insertAt = TooltipInjector.computeInsertIndex(tooltip, flag.isAdvanced());
+        TooltipInjector.renderShiftAlt(stack, tooltip, "tooltip.advancedrocketry.landingpad", insertAt);
     }
 
     @Override
