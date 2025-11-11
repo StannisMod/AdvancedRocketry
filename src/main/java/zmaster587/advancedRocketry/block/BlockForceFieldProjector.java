ec20523eeaa6293d686f5c7f1fd718a326be55f5
kaduvill <johansj@uia.no>
2025-11-11 01:23:34 +0100

    Implement tooltip for BlockForceFieldProjector
    Added tooltip information for BlockForceFieldProjector.

--- src/main/java/zmaster587/advancedRocketry/block/BlockForceFieldProjector.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockForceFieldProjector.java
@@ -1,10 +1,19 @@
 package zmaster587.advancedRocketry.block;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import zmaster587.advancedRocketry.client.TooltipInjector;
 import zmaster587.advancedRocketry.tile.TileForceFieldProjector;
 import zmaster587.libVulpes.block.BlockFullyRotatable;
 
@@ -32,6 +41,12 @@ public class BlockForceFieldProjector extends BlockFullyRotatable {
     @Override
     public TileEntity createTileEntity(World world, IBlockState state) {
         return new TileForceFieldProjector();
+    }
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag) {
+        int insertAt = TooltipInjector.computeInsertIndex(tooltip, flag.isAdvanced());
+        TooltipInjector.renderShiftAlt(stack, tooltip, "tooltip.advancedrocketry.forcefieldprojector", insertAt);
     }
 
 }
