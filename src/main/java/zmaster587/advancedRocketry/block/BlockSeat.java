808653c000898f7a2ebaa831d3c0f9d6cb900a6f
kaduvill <johansj@uia.no>
2025-11-17 09:06:15 +0100

    Fix formatting and add tooltip information for BlockSeat

--- src/main/java/zmaster587/advancedRocketry/block/BlockSeat.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockSeat.java
@@ -119,5 +119,4 @@ public class BlockSeat extends Block {
         int insertAt = TooltipInjector.computeInsertIndex(tooltip, flag.isAdvanced());
         TooltipInjector.renderShiftAlt(stack, tooltip, "tooltip.advancedrocketry.seat", insertAt);
     }
-    
 }
