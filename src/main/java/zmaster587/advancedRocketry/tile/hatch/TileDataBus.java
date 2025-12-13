76570b6dbea49f478f8fd0f0eb364f282d523626
kaduvill <johansj@uia.no>
2025-12-13 12:19:07 +0100

    Refactor TileDataBus to use IDataItem interface

--- src/main/java/zmaster587/advancedRocketry/tile/hatch/TileDataBus.java
+++ src/main/java/zmaster587/advancedRocketry/tile/hatch/TileDataBus.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import zmaster587.advancedRocketry.api.DataStorage;
 import zmaster587.advancedRocketry.api.DataStorage.DataType;
 import zmaster587.advancedRocketry.inventory.modules.ModuleAutoData;
+import zmaster587.advancedRocketry.item.IDataItem;
 import zmaster587.advancedRocketry.item.ItemData;
 import zmaster587.advancedRocketry.util.IDataInventory;
 import zmaster587.libVulpes.inventory.modules.ModuleBase;
@@ -22,17 +23,18 @@ import java.util.List;
 
 public class TileDataBus extends TileInventoryHatch implements IDataInventory, INetworkMachine {
 
-    DataStorage data;
+    protected DataStorage data;
+    protected static final int BASE_MAX_DATA = 2000;
 
     public TileDataBus() {
         data = new DataStorage(DataStorage.DataType.UNDEFINED);
-        data.setMaxData(2000);
+        data.setMaxData(BASE_MAX_DATA);
     }
 
     public TileDataBus(int number) {
         super(number);
         data = new DataStorage(DataStorage.DataType.UNDEFINED);
-        data.setMaxData(2000);
+        data.setMaxData(BASE_MAX_DATA);
 
         inventory.setCanInsertSlot(0, true);
         inventory.setCanInsertSlot(1, false);
@@ -45,13 +47,24 @@ public class TileDataBus extends TileInventoryHatch implements IDataInventory, I
 
         ItemStack itemStack = inventory.getStackInSlot(0);
 
-        if (itemStack != ItemStack.EMPTY && itemStack.getItem() instanceof ItemData) {
-            ItemData itemData = (ItemData) itemStack.getItem();
-            itemData.removeData(itemStack, this.data.addData(itemData.getData(itemStack), itemData.getDataType(itemStack), true), DataStorage.DataType.UNDEFINED);
+        if (!itemStack.isEmpty() && itemStack.getItem() instanceof IDataItem) {
+            IDataItem item = (IDataItem) itemStack.getItem();
+
+            DataStorage chip = item.getDataStorage(itemStack);
+
+            int moved = this.data.addData(
+                    chip.getData(),
+                    chip.getDataType(),
+                    true
+            );
+
+            // Remove exactly what was accepted
+            item.removeData(itemStack, moved, DataStorage.DataType.UNDEFINED);
 
             inventory.setInventorySlotContents(1, decrStackSize(0, 1));
         }
     }
+
 
     @Override
     public String getModularInventoryName() {
@@ -62,9 +75,19 @@ public class TileDataBus extends TileInventoryHatch implements IDataInventory, I
     public void storeData(int id) {
         ItemStack itemStack = inventory.getStackInSlot(0);
 
-        if (!itemStack.isEmpty() && itemStack.getItem() instanceof ItemData && inventory.getStackInSlot(1) == ItemStack.EMPTY) {
-            ItemData itemData = (ItemData) itemStack.getItem();
-            this.data.removeData(itemData.addData(itemStack, this.data.getData(), this.data.getDataType()), true);
+        if (!itemStack.isEmpty()
+                && itemStack.getItem() instanceof IDataItem
+                && inventory.getStackInSlot(1).isEmpty()) {
+
+            IDataItem item = (IDataItem) itemStack.getItem();
+
+            int added = item.addData(
+                    itemStack,
+                    this.data.getData(),
+                    this.data.getDataType()
+            );
+
+            this.data.removeData(added, true);
 
             inventory.setInventorySlotContents(1, decrStackSize(0, 1));
         }
@@ -126,22 +149,40 @@ public class TileDataBus extends TileInventoryHatch implements IDataInventory, I
     @Override
     public void setInventorySlotContents(int slot, @Nonnull ItemStack stack) {
         inventory.setInventorySlotContents(slot, stack);
+
         ItemStack itemStack = inventory.getStackInSlot(0);
 
-        if (itemStack != ItemStack.EMPTY && itemStack.getItem() instanceof ItemData && inventory.getStackInSlot(1) == ItemStack.EMPTY) {
-            ItemData itemData = (ItemData) itemStack.getItem();
-            if (itemData.getData(itemStack) > 0 && data.getData() != data.getMaxData()) {
+        if (!itemStack.isEmpty()
+                && itemStack.getItem() instanceof IDataItem
+                && inventory.getStackInSlot(1).isEmpty()) {
+
+            IDataItem item = (IDataItem) itemStack.getItem();
+            DataStorage chip = item.getDataStorage(itemStack);
+
+            int chipData = chip.getData();
+            int chipMax  = chip.getMaxData();
+
+            // Auto-load from chip -> bus
+            if (chipData > 0 && data.getData() < data.getMaxData()) {
                 loadData(0);
-            } else if (data.getData() != 0 && 1000 > itemData.getData(itemStack)) {
+
+            // Auto-store from bus -> chip
+            } else if (data.getData() > 0 && chipData < chipMax) {
                 storeData(0);
             }
         }
+
         inventory.markDirty();
         markDirty();
         this.handleUpdateTag(getUpdateTag());
 
         if (this.hasMaster() && this.getMasterBlock() instanceof TileMultiBlock)
             ((TileMultiBlock) this.getMasterBlock()).onInventoryUpdated();
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 1;
     }
 
     @Override
