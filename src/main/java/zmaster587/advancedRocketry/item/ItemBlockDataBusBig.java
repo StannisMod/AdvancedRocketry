bc1d84f6f24db804c37d46763a791a449cc07278
kaduvill <johansj@uia.no>
2025-12-13 12:16:37 +0100

    Add ItemBlockDataBusBig class for data storage

---
+++ src/main/java/zmaster587/advancedRocketry/item/ItemBlockDataBusBig.java
@@ -0,0 +1,150 @@
+package zmaster587.advancedRocketry.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import zmaster587.advancedRocketry.api.DataStorage;
+import zmaster587.advancedRocketry.tile.hatch.TileDataBusBig;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ItemBlockDataBusBig extends ItemBlock implements IDataItem {
+
+    // Keep this local and explicit
+    private static final int BIG_MAX_DATA = 2000 * 4;
+
+    public ItemBlockDataBusBig(Block block) {
+        super(block);
+        setHasSubtypes(false);
+        setMaxDamage(0);
+    }
+
+    // ---- IDataItem ----
+
+    @Override
+    public int getMaxData(@Nonnull ItemStack stack) {
+        return BIG_MAX_DATA;
+    }
+
+    @Override
+    @Nonnull
+    public DataStorage getDataStorage(@Nonnull ItemStack stack) {
+        DataStorage data = new DataStorage();
+
+        if (!stack.hasTagCompound()) {
+            data.setMaxData(getMaxData(stack));
+            data.setData(0, DataStorage.DataType.UNDEFINED);
+        } else {
+            data.readFromNBT(stack.getTagCompound());
+            data.setMaxData(getMaxData(stack));
+            if (data.getData() > data.getMaxData()) {
+                data.setData(data.getMaxData(), data.getDataType());
+            }
+        }
+        return data;
+    }
+
+    @Override
+    public int addData(@Nonnull ItemStack stack, int amount, @Nonnull DataStorage.DataType dataType) {
+        DataStorage data = getDataStorage(stack);
+
+        int added = data.addData(amount, dataType, true);
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        data.writeToNBT(nbt);
+        stack.setTagCompound(nbt);
+
+        return added;
+    }
+
+    @Override
+    public int removeData(@Nonnull ItemStack stack, int amount, @Nonnull DataStorage.DataType dataType) {
+        DataStorage data = getDataStorage(stack);
+
+        int removed = data.removeData(amount, true);
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        data.writeToNBT(nbt);
+        stack.setTagCompound(nbt);
+
+        return removed;
+    }
+
+    @Override
+    public void setData(@Nonnull ItemStack stack, int amount, @Nonnull DataStorage.DataType dataType) {
+        DataStorage data = getDataStorage(stack);
+
+        data.setData(amount, dataType);
+
+        NBTTagCompound nbt = new NBTTagCompound();
+        data.writeToNBT(nbt);
+        stack.setTagCompound(nbt);
+    }
+
+    // ---- stacking rule (same behavior as ItemData) ----
+    @Override
+    public int getItemStackLimit(@Nonnull ItemStack stack) {
+        return getData(stack) == 0 ? super.getItemStackLimit(stack) : 1;
+    }
+
+    // ---- place NBT into TE ----
+    @Override
+    public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, BlockPos pos,
+                                EnumFacing side, float hitX, float hitY, float hitZ, IBlockState newState) {
+
+        boolean placed = super.placeBlockAt(stack, player, world, pos, side, hitX, hitY, hitZ, newState);
+        if (!placed || world.isRemote) return placed;
+
+        if (!stack.hasTagCompound()) return placed;
+
+        TileEntity te = world.getTileEntity(pos);
+        if (!(te instanceof TileDataBusBig)) return placed;
+
+        TileDataBusBig bus = (TileDataBusBig) te;
+
+        NBTTagCompound tag = stack.getTagCompound();
+        if (tag != null) {
+            bus.getDataObject().readFromNBT(tag);
+            bus.markDirty();
+            world.notifyBlockUpdate(pos, newState, newState, 3);
+        }
+
+        return placed;
+    }
+
+    // ---- tooltip parity with ItemData ----
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(@Nonnull ItemStack stack, @Nullable World world,
+                               List<String> list, ITooltipFlag flag) {
+
+        DataStorage data = getDataStorage(stack);
+
+        // Header
+        list.add(I18n.format("tooltip.advancedrocketry.databusbig.header"));
+
+        // Type
+        String typeText = I18n.format(data.getDataType().toString());
+        list.add(I18n.format("tooltip.advancedrocketry.itemdata.type") + typeText);
+
+        // Data
+        list.add(I18n.format("tooltip.advancedrocketry.itemdata.data")
+                + TextFormatting.GOLD + data.getData()
+                + TextFormatting.WHITE + " / "
+                + TextFormatting.GOLD + data.getMaxData());
+    }
+}
