1a47e30714ba6e1bdb522d1aba3503e55d136d1b
jchung01 <jjchung276@gmail.com>
2026-02-23 15:43:22 -0800

    Remove HookLib and old async weather impl
    - The repacked HookLib is outdated and incompatible with Mixin if any mod mixins into the same classes
    - Just use Mixin or raw ASM if bytecode manipulation is needed

--- src/main/java/zmaster587/advancedRocketry/asm/AdvancedRocketryPlugin.java
+++ src/main/java/zmaster587/advancedRocketry/asm/AdvancedRocketryPlugin.java
@@ -3,24 +3,18 @@ package zmaster587.advancedRocketry.asm;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
-import zmaster587.advancedRocketry.ARHookLoader;
-import zmaster587.advancedRocketry.repack.gloomyfolken.hooklib.minecraft.HookLoader;
 
 import java.util.Map;
 
 @TransformerExclusions(value = {"zmaster587.advancedRocketry.asm.ClassTransformer"})
 @MCVersion("1.12.2")
 public class AdvancedRocketryPlugin implements IFMLLoadingPlugin {
-
-    private final HookLoader hookLoader;
-
     public AdvancedRocketryPlugin() {
-        hookLoader = new ARHookLoader();
     }
 
     @Override
     public String[] getASMTransformerClass() {
-        return new String[]{ClassTransformer.class.getName(), hookLoader.getASMTransformerClass()[0]};
+        return new String[]{ClassTransformer.class.getName()};
     }
 
     @Override
@@ -30,16 +24,15 @@ public class AdvancedRocketryPlugin implements IFMLLoadingPlugin {
 
     @Override
     public String getSetupClass() {
-        return hookLoader.getSetupClass();
+        return null;
     }
 
     @Override
     public void injectData(Map<String, Object> data) {
-        hookLoader.injectData(data);
     }
 
     @Override
     public String getAccessTransformerClass() {
-        return hookLoader.getAccessTransformerClass();
+        return null;
     }
 }
